fc888e16ae2399a1b3ed783b2913b5eda5a76425
bcuser <j.rao@bellevuecollege.edu>
2022-11-13 21:00:49 -0800

    Adds link from plant detail component to plant update form and back

--- src/app/plantupdateform/plantupdateform.component.ts
+++ src/app/plantupdateform/plantupdateform.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 import {UpdatePlantService} from '../update-plant.service'
 import { UpdatePlant } from '../updatePlant';
 @Component({
@@ -11,14 +13,19 @@ export class PlantupdateformComponent implements OnInit {
     plant: string[] = [];
     updatePlant: UpdatePlant;
     userId = sessionStorage.getItem('ID:');
+    plantID: string;
 
   
-    constructor(private updatePlantService: UpdatePlantService) { }
+    constructor(
+      private route: ActivatedRoute,
+      private updatePlantService: UpdatePlantService,
+      private location: Location
+    ) { }
   
     onClickSubmit(data) {
       console.log(data.plantID); //deleteme
       this.updatePlant = new UpdatePlant();
-      this.updatePlant.plantID = data.plantID;
+      this.updatePlant.plantID = this.plantID;
       console.log("plantID from form: "+this.updatePlant.plantID) //deleteme
       this.updatePlant.updateDate = data.date;
       this.updatePlant.updateCurrentHeight = +data.currentheight;
@@ -28,10 +35,11 @@ export class PlantupdateformComponent implements OnInit {
       console.log(this.updatePlant)
 
       this.updatePlantService.UpdatePlant(this.updatePlant).subscribe();
+      this.location.back();
    }
   
     ngOnInit(): void {
-      
+      this.plantID = this.route.snapshot.paramMap.get('id');
     }
 
 
