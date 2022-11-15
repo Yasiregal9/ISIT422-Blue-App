5c413dda6acc571d1ae11272dee2740f143bb515
bcuser <j.rao@bellevuecollege.edu>
2022-11-14 21:00:42 -0800

    Corrects the declaration of the plant info detal component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'plantregister', component: PlantregformComponent },
-  { path: 'weather', component: WeatheralertsComponent}
+  { path: 'weather', component: WeatheralertsComponent},
   { path: 'plantinfodetails/:id', component: InfoPlantDetailsComponent },
   { path: 'plantupdate/:id', component: PlantupdateformComponent },
   { path: 'plantinfo', component: PlantinfoComponent},
